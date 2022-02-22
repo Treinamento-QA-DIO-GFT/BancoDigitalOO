2dfc4ff18b5fe52732d5ebf30d38e172bfc8724b
Diego Resende <wdresende@gmail.com>
2022-02-22 15:58:17 -0300

    "Criando projeto de Banco com OO"

---
+++ src/ContaCorrente.java
@@ -0,0 +1,19 @@
+
+public class ContaCorrente {
+	
+	public int agencia;
+	public int numero;
+	public int saldo;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void transferir() {
+		
+	}
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,19 @@
+
+public class ContaPoupanca {
+
+	public int agencia;
+	public int numero;
+	public int saldo;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void transferir() {
+		
+	}
+}
